14314223c915a3ced5d802f655843bb0ea49798d
Leo Porto <leonardoreisporto@gmail.com>
2020-06-26 07:09:13 -0300

    Updates

---
+++ precache-manifest.dae12538e2a2f4503adc458e2f77fbd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84cb005a188919b5bdf455487a8220f4",
+    "url": "/fs-mixolidio/index.html"
+  },
+  {
+    "revision": "65fcad83f67c539b401a",
+    "url": "/fs-mixolidio/static/css/main.6db28dbe.chunk.css"
+  },
+  {
+    "revision": "fb4b1abc5a104ff993f8",
+    "url": "/fs-mixolidio/static/js/2.bba9dae4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fs-mixolidio/static/js/2.bba9dae4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65fcad83f67c539b401a",
+    "url": "/fs-mixolidio/static/js/main.e6129228.chunk.js"
+  },
+  {
+    "revision": "63e72a79cd7d80f0ff0b",
+    "url": "/fs-mixolidio/static/js/runtime-main.372f1e10.js"
+  }
+]);
